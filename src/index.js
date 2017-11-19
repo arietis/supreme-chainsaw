1a452ea6a71e58d36eb866b352e0725634040375
Sergei Guselnikov <guselnikov@me.com>
2017-11-19 17:50:38 +0300

    implemented rendering of tickets data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+const ticketsData = require('./tickets.json');
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class TicketCell extends React.Component {
+    render() {
+        var ticket = this.props.ticket;
+
+        return(
+            <div className="ticket-cell">
+                <div>Origin: {ticket.origin}</div>
+                <div>{ticket.origin_name}</div>
+                <div>Destination: {ticket.destination}</div>
+                <div>{ticket.destination_name}</div>
+                <div>Departure: {ticket.departure_date}</div>
+                <div>{ticket.departure_time}</div>
+                <div>Arrival: {ticket.arrival_date}</div>
+                <div>{ticket.arrival_time}</div>
+                <div>Carrier: {ticket.carrier}</div>
+                <div>Stops: {ticket.stops}</div>
+                <div>Price: {ticket.price}</div>
+            </div>
+        );
+    }
+}
+
+class TicketsList extends React.Component {
+    renderTicket(ticket) {
+        return <TicketCell ticket={ticket}/>
+    }
+
+    render() {
+        console.log(ticketsData);
+        return(
+            <div>
+                {ticketsData.tickets.map(ticket => this.renderTicket(ticket))}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <TicketsList />,
+    document.getElementById('root')
+);
